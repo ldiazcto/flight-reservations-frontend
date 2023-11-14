ef47feffde1bb7545bd56e0b13046455ee3219dc
ArelyAgama <aagama@fi.uba.ar>
2023-11-13 23:17:50 -0300

    conectandome

---
+++ app/src/main/java/com/tdl/flights/flightsapp/SearchFlightsActivity.kt
@@ -0,0 +1,13 @@
+package com.tdl.flights.flightsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tdl.flights.R
+
+class SearchFlightsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_flights)
+        //arranca mi pantalla
+    }
+}
