f78c7bf8ec2462919165ad7590c258c76203385b
ArelyAgama <aagama@fi.uba.ar>
2023-12-10 23:45:37 -0300

    add data information

--- app/src/main/java/com/tdl/flights/flightsapp/ProcessReservationActivity.kt
+++ app/src/main/java/com/tdl/flights/flightsapp/ProcessReservationActivity.kt
@@ -16,13 +16,17 @@ class ProcessReservationActivity : AppCompatActivity() {
         val etNombre = findViewById<AppCompatEditText>(R.id.etNombre) //para el id de mi editText(donde ingreo el valor)
         val etApellido = findViewById<AppCompatEditText>(R.id.etApellido)
         val etDNI = findViewById<AppCompatEditText>(R.id.etDNI)
+        val etTelefono = findViewById<AppCompatEditText>(R.id.etTelefono)
+        val etEmail = findViewById<AppCompatEditText>(R.id.etEmail)
 
         btnEnd.setOnClickListener{//lo que quiero hacer cuando se pulse el boton
             val nombre = etNombre.text.toString()
             val apellido = etApellido.text.toString()
             val DNI = etDNI.text.toString()
+            val telefono = etTelefono.text.toString()
+            val email = etEmail.text.toString()
 
-            if(nombre.isNotEmpty() and apellido.isNotEmpty() and DNI.isNotEmpty()){ //para moverme entre pantallas(activitys)
+            if(nombre.isNotEmpty() and apellido.isNotEmpty() and DNI.isNotEmpty() and telefono.isNotEmpty() and email.isNotEmpty()){ //para moverme entre pantallas(activitys)
                 //val intent = Intent(this, FlightOptionsActivity::class.java)
                 //intent.putExtra("EXTRA_NOMBRE", nombre)
                 //intent.putExtra("EXTRA_APELLIDO",apellido)
